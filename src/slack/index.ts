dd0ae0d1b9a1c4f430649c409c55670280d10d95
ycsong <ycsong@vibeon.ai>
2025-07-31 09:00:22 +0900

    refactor(slack): initialize boltApp with socketMode and add to export

--- src/slack/index.ts
+++ src/slack/index.ts
@@ -1,6 +1,7 @@
 import * as log from 'bog';
 import { WebClient } from '@slack/web-api';
 import { RTMClient } from '@slack/rtm-api';
+import { App } from '@slack/bolt';
 import { RTMMock, WebMock } from '../../test/lib/slackMock';
 import config from '../config';
 
@@ -12,3 +13,10 @@ export default {
     rtm: slackMock ? new RTMMock() : new RTMClient(config.slack.api_token),
     wbc: slackMock ? new WebMock() : new WebClient(config.slack.api_token),
 };
+
+export const boltApp = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
+});
